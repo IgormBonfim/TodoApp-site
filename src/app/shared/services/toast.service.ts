8f20e2493435f0a3fb6bb29431c5dfc4aefc92e7
IgorBonfim <igormbonfim@hotmail.com>
2023-02-13 19:21:15 -0300

    Adiciona metodo de sucesso ao toastrService

--- src/app/shared/services/toast.service.ts
+++ src/app/shared/services/toast.service.ts
@@ -6,11 +6,20 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ToastService {
 
+  private tempoDoToast = 3000;
+
   constructor(private toastr: ToastrService) { }
 
   public erro(titulo: string, mensagem: string) {
     this.toastr.error(mensagem, titulo, {
-      timeOut: 1000
-    })
+      timeOut: this.tempoDoToast
+    });
   }
+
+  public sucesso(titulo: string, mensagem: string) {
+    this.toastr.success(mensagem, titulo, {
+      timeOut: this.tempoDoToast
+    });
+  }
+
 }
